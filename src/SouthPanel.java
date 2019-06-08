c2dffef91061a38b424050b92d7dcd4f6d0065ed
Soroush Mehraban <46867790+SoroushMehraban@users.noreply.github.com>
2019-06-07 19:37:26 -0700

    Javadoc added to westpart panel for southpanel

--- src/SouthPanel.java
+++ src/SouthPanel.java
@@ -473,29 +473,41 @@ public class SouthPanel extends JPanel {
             });
         }
     }
+
+    /**
+     * This inner class is about west part panel of SouthPanel for Jpotify.
+     * it shows us 2 things: Song Name and Artist Name.
+     *
+     * @author Soroush Mehraban & Morteza Damghani
+     * @version 1.0
+     */
     private class WestPartPanel extends JPanel{
         private JLabel songName;
         private JLabel artistName;
         private BufferedImage plusIcon;
         private JLabel plusLabel;
 
+        /**
+         * Class Constructor.
+         * @throws IOException if opening plus icon failed.
+         */
         public WestPartPanel() throws IOException {
             //setting layout to Box layout and Line axis:
             this.setLayout(new GridBagLayout());
             GridBagConstraints constraints = new GridBagConstraints();
             //setting background:
             this.setBackground(Color.getHSBColor(0, 0, 0.16f));
-
+            //creating song name label:
             songName = new JLabel("Song Name");
             songName.setForeground(Color.getHSBColor(0, 0, 0.7f));
-
+            //creating artist name label:
             artistName = new JLabel("Artist Name");
             artistName.setForeground(Color.getHSBColor(0, 0.09f, 0.36f));
-
+            //creating plus label with icon:
             plusIcon = ImageIO.read(new File("Icons/Plus-no-select.png"));
             plusLabel = new JLabel(new ImageIcon(plusIcon));
             createplusLabelAction();
-
+            //putting components in panel:
             constraints.fill = GridBagConstraints.BOTH;
             constraints.insets = new Insets(5,5,0,0);
             constraints.gridx = 0;
@@ -509,6 +521,12 @@ public class SouthPanel extends JPanel {
             constraints.gridy = 1;
             this.add(artistName,constraints);
         }
+        /**
+         * This method demonstrate what happens if mouse pressed,entered or exited from plus Icon beside Song name.
+         * when mouse pressed: ...
+         * when mouse entered: it made that icon look brighter.
+         * when mouse exited: it changes to previous form.
+         */
         private void createplusLabelAction(){
             plusLabel.addMouseListener(new MouseAdapter(
             ) {
