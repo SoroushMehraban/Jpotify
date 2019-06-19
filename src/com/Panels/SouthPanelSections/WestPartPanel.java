46feb4c7460a765e56aa23172b147424b61e52a8
Soroush Mehraban <46867790+SoroushMehraban@users.noreply.github.com>
2019-06-19 10:44:48 +0430

    Action listener implemented to create animated text for text at left down corner if they're length is more than 15 character.

--- src/com/Panels/SouthPanelSections/WestPartPanel.java
+++ src/com/Panels/SouthPanelSections/WestPartPanel.java
@@ -21,8 +21,12 @@ import java.io.IOException;
 public class WestPartPanel extends JPanel {
     private JLabel songName;
     private JLabel artistName;
+    private String artistFullName;
+    private String songFullName;
     private BufferedImage plusIcon;
     private JLabel plusLabel;
+    private boolean isArtistEntered;
+    private boolean isSongEntered;
 
     /**
      * Class Constructor.
@@ -36,11 +40,15 @@ public class WestPartPanel extends JPanel {
         //setting background:
         this.setBackground(new Color(41,41,41));
         //creating song name label:
-        songName = new JLabel("Song Name");
+        songFullName = "Song Name";
+        songName = new JLabel(songFullName);
         songName.setForeground(new Color(179,179,179));
+        createSongNameAction();
         //creating artist name label:
-        artistName = new JLabel("Artist Name");
+        artistFullName = "Artist Name";
+        artistName = new JLabel(artistFullName);
         artistName.setForeground(new Color(92,84,84));
+        createArtistNameAction();
         //creating plus label with icon:
         plusIcon = ImageIO.read(new File("Icons/Plus-no-select.png"));
         plusLabel = new JLabel(new ImageIcon(plusIcon));
@@ -94,11 +102,86 @@ public class WestPartPanel extends JPanel {
             }
         });
     }
+
+    /**
+     * This method demonstrate what happens if mouse pressed or exited from Artist name at left down corner.
+     * when mouse entered: if it's length is more than 15 character, it moves in a animated way.
+     * when mouse exited: it changes to previous form.
+     */
+    private void createArtistNameAction(){
+        artistName.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                isArtistEntered = true;//this variable indicated when should thread in below terminate.
+                if(artistFullName.length() > 15){//if it's size is more than 15 character and doesn't fit
+                    Thread artistNameThread = new Thread(new Runnable() {//creating thread to move the text
+                        @Override
+                        public void run() {
+                            while(isArtistEntered)//while mouse is Entered
+                                for (int i = 0; i <artistFullName.length() - 15 && isArtistEntered ; i++) {
+                                    artistName.setText(artistFullName.substring(i,i+12));
+                                    try {
+                                        Thread.sleep(500);
+                                    } catch (InterruptedException e1) {
+                                        JOptionPane.showMessageDialog(null, "Artist name thread Interrupted");
+                                    }
+                                }
+                        }
+                    });
+                    artistNameThread.start();
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                artistName.setText(createShortenedText(artistFullName));//changing text to previous form
+                isArtistEntered = false;//closing thread if it's running
+            }
+        });
+    }
+    /**
+     * This method demonstrate what happens if mouse pressed or exited from Song name at left down corner.
+     * when mouse entered: if it's length is more than 15 character, it moves in a animated way.
+     * when mouse exited: it changes to previous form.
+     */
+    private void createSongNameAction(){
+        songName.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                isSongEntered = true;//this variable indicated when should thread in below terminate.
+                if(songFullName.length() > 15){//if it's size is more than 15 character and doesn't fit
+                    Thread songNameThread = new Thread(new Runnable() {//creating thread to move the text
+                        @Override
+                        public void run() {
+                            while(isSongEntered)//while mouse is Entered
+                                for (int i = 0; i <songFullName.length() - 15 && isSongEntered ; i++) {
+                                    songName.setText(songFullName.substring(i,i+12));
+                                    try {
+                                        Thread.sleep(500);
+                                    } catch (InterruptedException e1) {
+                                        JOptionPane.showMessageDialog(null, "Song name thread Interrupted");
+                                    }
+                                }
+                        }
+                    });
+                    songNameThread.start();
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                songName.setText(createShortenedText(songFullName));//changing text to previous form
+                isSongEntered = false;//closing thread if it's running
+            }
+        });
+    }
     public void updateNames(String directory){
         try {
             MP3Info mp3Info = new MP3Info(directory);
-            String artist = createShortenedText(mp3Info.getArtist());
-            String songTitle = createShortenedText(mp3Info.getTitle());
+            artistFullName = mp3Info.getArtist();
+            songFullName = mp3Info.getTitle();
+            String artist = createShortenedText(artistFullName);
+            String songTitle = createShortenedText(songFullName);
             artistName.setText(artist);
             songName.setText(songTitle);
         } catch (IOException e) {
