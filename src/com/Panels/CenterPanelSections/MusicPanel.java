4d93d2f9fb8df463eab48ad596c506b871163138
Soroush Mehraban <46867790+SoroushMehraban@users.noreply.github.com>
2019-06-19 13:59:19 +0430

    MusicPanel constructors created, now we can have a musicPanel which is a library or playlist

--- src/com/Panels/CenterPanelSections/MusicPanel.java
+++ src/com/Panels/CenterPanelSections/MusicPanel.java
@@ -4,17 +4,12 @@ import com.GUIFrame.GUIFrame;
 import com.MP3.MP3Info;
 import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.UnsupportedTagException;
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.Player;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
@@ -32,38 +27,58 @@ class MusicPanel extends JPanel{
     private MP3Info mp3Info;
 
     /**
-     * class constructor
-     * @param mp3Info information about mp3 we want to play
-     * @param description description to show under the title.
-     * @throws InvalidDataException
-     * @throws IOException
-     * @throws UnsupportedTagException
+     * a constructor which has common features between Library and music
+     * @param description description to show
      */
-    MusicPanel(MP3Info mp3Info, String description) throws InvalidDataException, IOException, UnsupportedTagException {
-        this.mp3Info = mp3Info;//setting object's mp3 info, helping us for listener method.
+    private MusicPanel(BufferedImage image,String title, String description){
         this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));//setting panel layout
         this.setBackground(new Color(23,23,23));//setting panel default background
         this.setPreferredSize(new Dimension(200,270));//setting panel's preferred size
-        createMusicPanelListener();//creating panel listener
 
-        showingImage = mp3Info.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);//making a specified size for image
+        showingImage = image.getScaledInstance(200, 200, Image.SCALE_SMOOTH);//making a specified size for image
         this.musicLabel = new JLabel(new ImageIcon(showingImage));//creating a label to show image
 
         this.title = new JLabel();//creating title label
+        this.title.setText(title);//setting title text
         this.title.setForeground(new Color(219,219,219));//setting title color
-        this.title.setText(mp3Info.getTitle());//setting title text
-
-        String stringModified = description.replace("\n","<br/>");//fixing all \n with <br/> as a line breaker.
-        description ="<html><p style=\"width:170px\">"+stringModified+"</p></html>";//creating a paragraph width which line breaks after that.
-
 
         this.description = new JLabel();//creating description label
+        String stringModified = description.replace("\n","<br/>");//fixing all \n with <br/> as a line breaker.
+        description ="<html><p style=\"width:170px\">"+stringModified+"</p></html>";//creating a paragraph width which line breaks after that.
         this.description.setText(description);//setting description text
         this.description.setForeground(new Color(120,120,120));//setting description color
+
         //adding components to panel:
         this.add(this.musicLabel);
         this.add(this.title);
         this.add(this.description);
+
+    }
+
+    /**
+     * if our panel is going to show a single music, this constructor should be called.
+     *
+     * @param mp3Info information about mp3 we want to play
+     * @param description description to show under the title.
+     */
+    MusicPanel(MP3Info mp3Info, String description) throws InvalidDataException, IOException, UnsupportedTagException {
+        this(mp3Info.getImage(), mp3Info.getTitle(), description);//calling private constructor to set common features.
+        this.mp3Info = mp3Info;//setting object's mp3 info, helping us for listener method.
+
+        createMusicListener();//creating panel listener
+
+    }
+
+    /**
+     * this constructor is called when panel is library or it's playlist
+     * @param image image to show at top of panel
+     * @param title title to show under the image
+     * @param description description to show under the title
+     * @param a duty of this panel
+     */
+    MusicPanel(BufferedImage image, String title, String description, MouseListener a){
+        this(image,title,description);//calling private constructor to set common features.
+        this.addMouseListener(a);
     }
 
     /**
@@ -72,7 +87,7 @@ class MusicPanel extends JPanel{
      * -when mouse entered: it changes to a brighter color.
      * -when mouse exited: it backs to previous color it had.
      */
-    private void createMusicPanelListener(){
+    private void createMusicListener(){
         this.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
