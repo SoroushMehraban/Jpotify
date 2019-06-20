98ed725553e748627551429716e924247cecb6b9
Soroush Mehraban <46867790+SoroushMehraban@users.noreply.github.com>
2019-06-20 20:12:13 +0430

    LikeLinker interface implemented to link playPanel in south panel with center part of centerPanel

---
+++ src/com/Interfaces/LikeLinker.java
@@ -0,0 +1,21 @@
+package com.Interfaces;
+
+
+/**
+ * and interface that links center part of center panel with play panel in south panel.
+ *
+ * @author Soroush Mehraban & Morteza Damghani
+ * @version 1.0
+ */
+public interface LikeLinker {
+    /**
+     * when like button pressed. if it wasn't press before, this method adds music to favorite play list.
+     * @param directory directory of current music.
+     */
+    void addToFavoritePlayList(String directory);
+    /**
+     * when like button pressed. if it was press before, this method adds music to favorite play list.
+     * @param directory directory of current music.
+     */
+    void removeFromFavoritePlayList(String directory);
+}
