4ad075beac76c2cea721ecc44d23fcf585251a21
Morteza-Damghani-Nouri <47036864+Morteza-Damghani-Nouri@users.noreply.github.com>
2019-06-25 14:38:04 +0430

    a JComboBox added.

--- src/com/Panels/CenterPanelSections/NorthPart.java
+++ src/com/Panels/CenterPanelSections/NorthPart.java
@@ -5,6 +5,9 @@ import com.Interfaces.SearchLinker;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Vector;
 
 /**
  * north part of center panel. it has this features:
@@ -30,5 +33,22 @@ public class NorthPart extends JPanel {
         searchBox = new SearchBox(10,searchLinker);//creating searchbox
         searchBox.setForeground(new Color(168,168,168));//setting search box text color
         this.add(searchBox);//adding search box beside left arrow label
+        this.add(Box.createHorizontalStrut(900));
+        JComboBox<JLabel> usersBox=new JComboBox<>();
+        //usersBox.setBounds();
+        //usersBox.getEditor().getEditorComponent().setBackground(new Color(23,23,23));
+        //JLabel temp=new JLabel("USER");
+        //users.add(temp);
+        //usersBox.setVisible(true);
+        //usersBox.add(temp);
+        //usersBox.setEditable(true);
+        //usersBox.setBackground(new Color(23,23,23));
+        //usersBox.setPrototypeDisplayValue("USERS");
+        //usersBox.setBounds(50,50,50,50);
+        this.add(usersBox);
+
+
+
+
     }
 }
